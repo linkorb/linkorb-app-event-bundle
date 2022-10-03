bf5a4f49c0441b027afca767fe7da8bbff6467d5
boite <boite@users.noreply.github.com>
2022-10-03 20:09:43 +0100

    feat: allow symfony 6 components

--- src/LinkORBAppEventBundle.php
+++ src/LinkORBAppEventBundle.php
@@ -3,6 +3,7 @@
 namespace LinkORB\AppEventBundle;
 
 use LinkORB\AppEventBundle\DependencyInjection\LinkORBAppEventExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LinkORBAppEventBundle extends Bundle
@@ -11,7 +12,7 @@ class LinkORBAppEventBundle extends Bundle
      * Implemented here to allow this bundle's Extension to define a custom
      * alias "linkorb_app_event" ("link_orb_app_event" is the expected alias).
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new LinkORBAppEventExtension();
